eee8e68c2535b1e52cb9eda3b31edee04f4be08e
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2024-04-17 13:44:30 +0200

    Bump version

--- src/mlchemad/__init__.py
+++ src/mlchemad/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-__version__ = '1.5.1'
+__version__ = '1.5.2'
 
 
 from .applicability_domains import (BoundingBoxApplicabilityDomain, CentroidDistanceApplicabilityDomain,
